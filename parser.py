536d9d3e8c43d8b7e6749d4823a3ecb5c1fcdd79
Tribusean Ilya <slash3b@gmail.com>
2018-11-08 22:56:10 +0200

    parser update

--- parser.py
+++ parser.py
@@ -7,32 +7,33 @@ from bs4 import BeautifulSoup
 base = datetime.datetime.today()
 dates = [base + datetime.timedelta(days=x) for x in range(0,6)]
 
-multiplex_id = 24
+cinema_ids = {"Multiplex":24, "Loteanu":36}
 # today = datetime.datetime.now().strftime("%d-%m-%Y")
 
 result = []
-
-for date in dates:
-    movieDay = date.strftime("%d-%m-%Y")
-    url = "http://patria.md/beta/wp-admin/admin-ajax.php?date=" + movieDay + "&cinema=" + str(multiplex_id) + "&action=flotheme_load_movies_scheduler"
-    response = requests.get(url)
-    soup = BeautifulSoup(response.text, 'html.parser')
-    moviesHtml = soup.find('div', 'sidebar-scheduler-movies') 
-    if moviesHtml:
-        movies = []
-        for link in soup.find_all("div", "title"):
-            title = link.a.contents[0]
-            if ("EN" in title):
-                item = {
-                    "title" : title,
-                    "time" : link.parent.find("div", "sessions").span.contents[0],
-                }
-                movies.append(item)
-        if movies:
-            result.append({
-                'date' : movieDay,
-                'movies' : movies,
-            })
+for cinema_name in cinema_ids:
+    for date in dates:
+        movieDay = date.strftime("%d-%m-%Y")
+        url = "http://patria.md/beta/wp-admin/admin-ajax.php?date=" + movieDay + "&cinema=" + str(cinema_ids[cinema_name]) + "&action=flotheme_load_movies_scheduler"
+        response = requests.get(url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        moviesHtml = soup.find('div', 'sidebar-scheduler-movies') 
+        if moviesHtml:
+            movies = []
+            for link in soup.find_all("div", "title"):
+                title = link.a.contents[0]
+                if ("EN" in title):
+                    item = {
+                        "title" : title,
+                        "time" : link.parent.find("div", "sessions").span.contents[0],
+                        "cinema" : cinema_name
+                    }
+                    movies.append(item)
+            if movies:
+                result.append({
+                    'date' : movieDay,
+                    'movies' : movies,
+                })
 
 if result:
     try:
