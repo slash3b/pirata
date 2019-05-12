b4007f918cc1aa4d2dadb5dc3e702a761dd7c293
ageispolis <me@ageispolis.com>
2019-05-12 09:14:25 +0000

    clean up

--- flask/app.py
+++ flask/app.py
@@ -61,3 +61,6 @@ def index():
 
     return render_template('index.html', upcoming=upcoming, playing=playing)
 
+if __name__ == "__main__":
+        pirata.run()
+
---
+++ flask/wsgi.py
@@ -0,0 +1,5 @@
+from app import pirata
+
+if __name__ == '__main__':
+    pirata.run()
+
