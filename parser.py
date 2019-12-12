52cf94047aa411eebaceb802098b7ba5290058f7
Ilya Tribusean <slash3b@gmail.com>
2019-12-12 20:43:20 +0200

    heavy rewrite for parser

--- parser.py
+++ parser.py
@@ -9,25 +9,6 @@ from parser import upcoming, playing, mail
 
 cn = connect('pirata.db')
 
-# coming soon section
-patria_url = 'http://patria.md/movies'
-response = requests.get(patria_url)
-soup = BeautifulSoup(response.text, 'html.parser')
-
-# since html markup is a piece of shit like the rest of the patria we take all the items from the second 'movies-page' div
-upcoming_movies = soup.find_all('div', 'page movies-page')[1].find_all('div', 'movies-item')
-for item in upcoming_movies:
-    attrs = item.find('a').attrs
-    if ("EN" in attrs['title']):
-        movie_title = attrs['title'].split('(')[0].rstrip()
-        movie_url = attrs['href']
-        # in order to get premiere date we have to visit each page, that sucks
-        response = requests.get(movie_url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        # todo: way to improve is to make request and find out if we already have that film in the database ?
-        premiere_date = soup.find_all('div', class_='premiere')[-1].contents[1] 
-        upcoming.register(cn, movie_title, premiere_date)
-
 # playing now section
 base = datetime.today()
 dates = [base + timedelta(days=x) for x in range(0,6)]
@@ -36,31 +17,49 @@ dates = [base + timedelta(days=x) for x in range(0,6)]
 cn.cursor().execute('DELETE FROM schedule WHERE datetime >=?', (datetime.today().strftime('%Y-%m-%d'),))
 cn.commit()
 
-for date in dates:
-    movieDay = date.strftime("%d-%m-%Y")
-    # cinema 24 stands for Mall cinema
-    # cinema 36 stands for Loteanu cinema
-    for cinema_id in ['24', '36']:
-        url = f'http://patria.md/beta/wp-admin/admin-ajax.php?date={movieDay}&cinema={cinema_id}&action=flotheme_load_movies_scheduler'
-        response = requests.get(url)
-        soup = BeautifulSoup(response.text, 'html.parser')
-        moviesHtml = soup.find('div', 'sidebar-scheduler-movies') 
-        if moviesHtml:
-            for link in soup.find_all("div", "title"):
-                title = link.a.contents[0]
-                if ("EN" in title):
-                    # skip new lines and return only html elements
-                    schedule_content =  [x for x in link.parent.find("div", "sessions").children if x != '\n']
-                    schedule = []
-                    for item in schedule_content:
-                        if item:
-                            # get time string
-                            time = item.contents[0].strip('/ ')
-                            date = datetime.strptime(movieDay + ' ' + time, '%d-%m-%Y %H:%M')
-                            schedule.append(date)
-                    movie_title = title.split('(')[0].rstrip()
-                    playing.register(cn, movie_title, cinema_id, schedule)
+client = requests.Session()
+client.get('https://patria.md/lang/en')
 
+cinema_map = {
+       'Multiplex' : 24, 
+       'Loteanu' : 36
+}
+
+for date in dates:
+    # parse path for one day
+    # e.g. https://patria.md/films?c_date=2019-12-16
+    movieDay = date.strftime('%Y-%m-%d')
+
+    url = f'https://patria.md/films?c_date={movieDay}'
+    response = client.get(url)
+    soup = BeautifulSoup(response.text, 'html.parser')
+
+    movies = soup.find('div', attrs={'id':'afisha1_'}).children
+
+    for movie in movies:
+        # exclude the fucking banner
+        movie_title = movie.find('div', class_='name')
+        if movie_title == None:
+            continue
+        movie_title = movie_title.contents[0]
+        if ('(EN)' in movie_title):
+            # MOVIE TITLE
+            movie_title = movie_title.split('(')[0].rstrip()
+
+            for cinema in movie.find_all('div', class_='h4'):
+                # CINEMA ID
+                cinema_id = cinema_map[cinema.contents[0].rstrip(':')]
+                # times for this particular cinema 
+
+                # find here all schedules
+                schedule = []
+                for times in cinema.find_next().find_all('a'):
+                    # e.g. 16:40
+                    time = times.find('span').contents[0]
+                    date = datetime.strptime(movieDay + ' ' + time, '%Y-%m-%d %H:%M')
+                    schedule.append(date)
+
+                playing.register(cn, movie_title, cinema_id, schedule)
 
 # my god, and this human have "ambitions"! Horrible, just horrible
 result = cn.cursor().execute('select title from films where date(register_date) = ?', (datetime.today().strftime('%Y-%m-%d'),))
