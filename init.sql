d369f8c0dcc1f22a8443a08912ea22fba105136f
Ilya Tribusean <slash3b@gmail.com>
2022-03-29 01:24:15 +0300

    some clean up again

--- init.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE films (id INTEGER PRIMARY KEY, title TEXT, meta TEXT, register_date TEXT);
-CREATE TABLE upcoming (id INTEGER PRIMARY KEY, film_id INTEGER, premiere_date TEXT);
-CREATE TABLE schedule (id INTEGER PRIMARY KEY, film_id INTEGER, location_id INTEGER, datetime TEXT);
