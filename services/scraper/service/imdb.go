e840fb31fb084aec5dac11a5d615867ec42926ce
Ilya Tribusean <slash3b@gmail.com>
2022-04-01 01:25:17 +0300

    imdb service

---
+++ services/scraper/service/imdb.go
@@ -0,0 +1,159 @@
+package service
+
+import (
+	"errors"
+	"fmt"
+	"net/http"
+	"net/url"
+	"scraper/storage/model"
+	"strings"
+
+	"github.com/anaskhan96/soup"
+)
+
+type IMDB struct {
+	c *http.Client
+}
+
+func NewIMDB(httpClient *http.Client) *IMDB {
+	imdb := IMDB{
+		c: httpClient,
+	}
+
+	return &imdb
+}
+
+/*
+	Ideas:
+	1. so do this getting concurrently
+	2. implement LRU cache in case movie was already searched for !
+
+*/
+
+type IMDBData struct {
+	poster  string
+	plot    string
+	runtime string
+	genres  string
+}
+
+func (c *IMDB) search(title string) (soup.Root, error) {
+
+	vals := url.Values{}
+	vals.Add("q", title)
+
+	path := fmt.Sprintf("https://www.imdb.com/find?%s", vals.Encode())
+
+	out, err := soup.GetWithClient(path, c.c)
+	if err != nil {
+		// log here error
+		return soup.Root{}, err
+	}
+
+	root := soup.HTMLParse(out)
+	allMovies := root.FindAll("tr", "class", "findResult")
+
+	if len(allMovies) == 0 {
+		// if movie info is missing
+		// log it here to be checked later on
+		// log.With("path", path) ... attempted to search for film but could not
+		return soup.Root{}, err
+	}
+
+	firstRow := allMovies[0]
+
+	linkNode := firstRow.Find("a")
+	if linkNode.Error != nil {
+		return soup.Root{}, err
+	}
+
+	attributes := linkNode.Attrs()
+
+	movieTitleLink, ok := attributes["href"]
+	if !ok {
+		// log with ... ooops smth happened
+		return soup.Root{}, errors.New("could not find link to movie")
+	}
+
+	path = fmt.Sprintf("https://www.imdb.com/%s", movieTitleLink)
+
+	res, err := soup.GetWithClient(path, c.c)
+	if err != nil {
+		// again log error here
+		return soup.Root{}, err
+	}
+
+	return soup.HTMLParse(res), nil
+}
+
+func (c *IMDB) GetFilmData(film model.Film) IMDBData {
+
+	var data IMDBData
+
+	root, err := c.search(film.Title)
+	if err != nil {
+		return data
+	}
+
+	// Poster
+	poster := root.Find("div", "class", "ipc-poster")
+	if poster.Error == nil {
+		posterImgEl := poster.Find("img")
+		if posterImgEl.Error == nil {
+			bb := posterImgEl.Attrs()
+
+			posterURL, ok := bb["src"]
+			if ok {
+				data.poster = posterURL
+			}
+		}
+	}
+
+	// Plot
+	plotElem := root.Find("p", "data-testid", "plot")
+	if plotElem.Error == nil {
+		plot := plotElem.Find("span")
+		if plot.Error == nil {
+			data.plot = plot.FullText()
+		}
+	}
+
+	// Runtime
+	techElem := root.Find("div", "data-testid", "title-techspecs-section")
+	if techElem.Error == nil {
+		techSpecs := techElem.FindAll("li")
+
+		for _, v := range techSpecs {
+			if v.Error == nil {
+				techSpecName := v.Find("span")
+				if techSpecName.Error == nil {
+					if strings.TrimSpace(strings.ToLower(techSpecName.Text())) == "runtime" {
+						content := v.Find("div")
+						if content.Error == nil {
+							data.runtime = content.FullText()
+							break
+						}
+					}
+				}
+			}
+		}
+	}
+
+	// Genres
+	genreTags := []string{}
+	genres := root.Find("li", "data-testid", "storyline-genres")
+	if genres.Error == nil {
+		results := genres.FindAll("a")
+
+		for _, v := range results {
+			if v.Error == nil {
+				// check that v is okay ?
+				genreTags = append(genreTags, v.FullText())
+			}
+		}
+
+		data.genres = strings.Join(genreTags, ", ")
+	}
+
+	return data
+}
