7ef807e08174d23cc6f940a8c7add9c9b13f9b3e
Ilya Tribusean <slash3b@gmail.com>
2022-06-02 21:41:45 +0300

    another variation over graceful shutdown for imdb service

--- services/imdb/main.go
+++ services/imdb/main.go
@@ -3,23 +3,41 @@ package main
 import (
 	"common/client"
 	"common/proto"
+	"context"
 	"fmt"
 	"imdb/metrics"
 	"imdb/service"
 	"log"
 	"net"
 	"net/http"
-
-	"google.golang.org/grpc"
+	"os"
+	"os/signal"
+	"syscall"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"google.golang.org/grpc"
 )
 
 func main() {
 
-	metricsStart()
+	ctx := context.Background()
+	ctx, stop := signal.NotifyContext(ctx, os.Interrupt, syscall.SIGTERM, syscall.SIGQUIT)
+	defer stop()
 
+	errCh := make(chan error, 1)
+	go metricsStart(errCh)
+	go grcpStart(errCh)
+
+	select {
+	case <-ctx.Done():
+		fmt.Println("exiting due to system signal received ...")
+	case e := <-errCh:
+		fmt.Println("error received, exiting, ", e)
+	}
+}
+
+func grcpStart(errCh chan<- error) {
 	listener, err := net.Listen("tcp", ":50051")
 	if err != nil {
 		panic(err)
@@ -27,37 +45,35 @@ func main() {
 
 	grpcSrv := grpc.NewServer()
 
-	//c := cache.NewLRU[string, dto.IMDBData](20)
-
 	cln, err := client.NewHttpClientWithCookies()
 	if err != nil {
-		panic(err)
+		errCh <- err
+		return
 	}
 	imdbService := service.NewIMDB(cln)
 
 	proto.RegisterIMDBServer(grpcSrv, service.NewIMDBGrpc(imdbService))
 
-	fmt.Println("started GRPC server on port 50051")
+	log.Println("started GRPC server on port 50051")
 	err = grpcSrv.Serve(listener)
 	if err != nil {
-		panic(err)
+		errCh <- err
+		return
 	}
-
 }
 
-func metricsStart() {
-
+func metricsStart(errCh chan<- error) {
 	prometheus.MustRegister(
 		metrics.HitMissCache,
 		metrics.CacheEvent,
 	)
 
-	go func() {
-		http.Handle("/metrics", promhttp.Handler())
-		log.Println("metrics available on :2112/metrics")
-		err := http.ListenAndServe(":2112", nil)
-		if err != nil {
-			log.Println(fmt.Errorf("unable to start metrics %v", err))
-		}
-	}()
+	http.Handle("/metrics", promhttp.Handler())
+	log.Println("metrics available on :2112/metrics")
+
+	err := http.ListenAndServe(":2112", nil)
+	if err != nil {
+		log.Println(fmt.Errorf("unable to start metrics %v", err))
+		errCh <- err
+	}
 }
