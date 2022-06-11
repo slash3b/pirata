303fbe96112ef4743764fa4becaf0689044ad4e7
Ilya Tribusean <slash3b@gmail.com>
2022-06-11 22:52:53 +0300

    some more horrible tests

--- services/scraper/service/scraper_test.go
+++ services/scraper/service/scraper_test.go
@@ -7,6 +7,7 @@ import (
 	mock_service "scraper/service/mock"
 	"scraper/storage/model"
 	"testing"
+	"time"
 
 	"github.com/golang/mock/gomock"
 	"github.com/stretchr/testify/assert"
@@ -14,7 +15,7 @@ import (
 
 //go:generate mockgen -source ./scraper.go -destination ./mock/scraper_mock.go
 
-func TestScraper_GetAllFilms_WithMixedResults(t *testing.T) {
+func TestScraper_GetAllFilms_WithNewResults(t *testing.T) {
 	mockCtrl := gomock.NewController(t)
 
 	mockSoup := mock_service.NewMockSoup(mockCtrl)
@@ -31,12 +32,9 @@ func TestScraper_GetAllFilms_WithMixedResults(t *testing.T) {
 		},
 	}, nil).Times(1)
 
-	gomock.InOrder(
-		mockRepo.EXPECT().IsExists(gomock.Any()).Return(false).Times(1),
-		mockRepo.EXPECT().IsExists(gomock.Any()).Return(true).Times(1),
-	)
+	mockRepo.EXPECT().IsExists(gomock.Any()).Return(false).Times(2)
 
-	mockRepo.EXPECT().Insert(gomock.Any()).Return(model.Film{}, nil).Times(1)
+	mockRepo.EXPECT().Insert(gomock.Any()).Return(model.Film{}, nil).Times(2)
 
 	scraper := service.NewScraper(mockRepo, mockSoup)
 
@@ -47,6 +45,60 @@ func TestScraper_GetAllFilms_WithMixedResults(t *testing.T) {
 		filmsCollection = append(filmsCollection, f)
 	}
 
-	assert.Len(t, filmsCollection, 1)
-	assert.NotEmpty(t, filmsCollection[0].Title)
+	location, err := time.LoadLocation("Europe/Chisinau")
+	assert.NoError(t, err)
+
+	expectedFilms := []model.Film{
+		{
+			ID:        0,
+			Title:     "Foo Bar",
+			Lang:      "KO",
+			Dimension: "2D",
+			StartDate: time.Date(2022, time.March, 10, 0, 0, 0, 0, location),
+		},
+		{
+			ID:        0,
+			Title:     "Moo Bar",
+			Lang:      "EN",
+			Dimension: "2D",
+			StartDate: time.Date(2022, time.March, 10, 0, 0, 0, 0, location),
+		},
+	}
+
+	assert.Len(t, filmsCollection, 2)
+
+	for _, c := range expectedFilms {
+		assert.Contains(t, filmsCollection, c)
+	}
+}
+
+func TestScraper_GetAllFilms_WithAlreadyExistingResults(t *testing.T) {
+	mockCtrl := gomock.NewController(t)
+
+	mockSoup := mock_service.NewMockSoup(mockCtrl)
+	mockRepo := mock_service.NewMockFilmStorageRepository(mockCtrl)
+
+	mockSoup.EXPECT().GetMovies().Return([]dto.RawFilmData{
+		{Title: "Foo Bar ",
+			Lang: "KO",
+			Date: "(10.03.2022)",
+		},
+		{Title: "Moo Bar ",
+			Lang: "EN",
+			Date: "(10.03.2022)",
+		},
+	}, nil).Times(1)
+
+	mockRepo.EXPECT().IsExists(gomock.Any()).Return(true).Times(2)
+
+	scraper := service.NewScraper(mockRepo, mockSoup)
+
+	_, films := scraper.GetFilms(context.Background())
+
+	var filmsCollection []model.Film
+	for f := range films {
+		filmsCollection = append(filmsCollection, f)
+	}
+
+	assert.Empty(t, filmsCollection)
 }
