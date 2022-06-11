5a388ae62975e84ffa29b384ff8d11aa115c9d18
Ilya Tribusean <slash3b@gmail.com>
2022-06-11 17:32:14 +0300

    lets try a simple sh script to do the checking

---
+++ .github/workflows/qc.sh
@@ -0,0 +1,53 @@
+#! /usr/bin/env sh
+
+set -e 
+
+ROOT=$(pwd)
+
+go install honnef.co/go/tools/cmd/staticcheck@latest
+
+echo "------------------- Checking API service"
+cd $ROOT/services/api
+
+echo "go test:"
+go test ./...
+echo "go vet:"
+go vet ./...
+echo "staticcheck:"
+staticcheck
+
+cd $ROOT
+
+echo "------------------- Checking common library"
+cd $ROOT/services/common
+
+echo "go test:"
+go test ./...
+echo "go vet:"
+go vet ./...
+
+cd $ROOT
+
+echo "------------------- Checking IMDB service"
+cd $ROOT/services/imdb
+
+echo "go test:"
+go test ./...
+echo "go vet:"
+go vet ./...
+echo "staticcheck:"
+staticcheck
+
+cd $ROOT
+
+echo "------------------- Checking Scraper service"
+cd $ROOT/services/scraper
+
+
+echo "go test:"
+go test ./...
+echo "go vet:"
+go vet ./...
+echo "staticcheck:"
+staticcheck
+
